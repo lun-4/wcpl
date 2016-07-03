f50f8ba4c3b49394f0c3ea6addc699c45743ef03
Luna Mendes <lkmnds@gmail.com>
2016-07-03 19:27:27 +1000

    Add compilation from wcpl to html

--- wcpl.py
+++ wcpl.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python3
 
+import sys
+argv = sys.argv
+
 WCPL_VERSION = '0.0.1'
 WCPL_BUILD = 1
 
 def wcpl(data, env):
 	compiled = []
+	env['_head'] = ''
 
 	for line in data.split('\n'):
+		if len(line) < 1:
+			continue
 		cmd = line[0]
 		commands = line.split(' ')
 		if commands[0] == 'm':
 			meta_type = commands[1]
+			meta_command = (''.join(commands[2:])).split('=')
+			meta_var = meta_command[0]
+			meta_val = meta_command[1]
+
+			print(meta_command)
 			if meta_type == 's':
-				
+				# read string
+				meta_val = meta_val[1:-1]
+				env['meta'][meta_var] = meta_val
 
+			env['_head'] += '<meta %s=%s>' % (meta_var, meta_val)
+		if commands[0] == 't':
+			compiled.append("%s" % ''.join(commands[1:]))
 
-	return '<html>%s</html>' % ''.join(compiled)
+	res = ''
+
+	res += '<head>\n %s \n</head>\n' % ''.join(env['_head'])
+	res += '\n<body>\n %s \n</body>' % '\n'.join(compiled)
+
+	return '<html>\n%s\n</html>' % res
 
 def main():
 	if len(argv) > 1:
@@ -32,7 +53,7 @@ def main():
 			}
 		}
 		with open(fname, 'r') as f:
-			wcpl(f.read(), env)
+			print(wcpl(f.read(), env))
 
 if __name__ == '__main__':
 	main()
